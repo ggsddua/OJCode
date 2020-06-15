95b9f99e22c1de367d4e07c1f0cc65c44980053e
shuai.yu_home <shuaiyu0702@foxmail.com>
2020-06-16 01:24:34 +0800

    骨牌覆盖问题框架

---
+++ HihoCoder/src/main/java/shuai/yu/DominoCoverage.java
@@ -0,0 +1,35 @@
+package shuai.yu;
+
+/**
+ * 骨牌覆盖问题
+ *
+ * @author shuai.yu
+ * @version 2020/06/16
+ */
+public class DominoCoverage
+{
+    /**
+     * 骨牌，一种古老的玩具。今天我们要研究的是骨牌的覆盖问题：
+     * 我们有一个2xN的长条形棋盘，然后用1x2的骨牌去覆盖整个棋盘。对于这个棋盘，一共有多少种不同的覆盖方法呢？
+     */
+    private void solution1()
+    {
+
+    }
+
+    /**
+     * 对于3xN的棋盘，使用1x2的骨牌去覆盖一共有多少种不同的覆盖方法呢
+     */
+    private void solution2()
+    {
+
+    }
+
+    /**
+     * 再加强一次题目，对于给定的K和N，我们需要去求KxN棋盘的覆盖方案数
+     */
+    private void solution3()
+    {
+
+    }
+}
