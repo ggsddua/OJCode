341e4c9dbb0d5acfffcfbf0e1518b6c6789b6c29
shuai.yu <1694640203@qq.com>
2020-10-14 20:32:32 +0800

    [Leetcode] 5 动态规划

--- Leetcode/src/main/java/shuai/yu/LongestPalindromicSub_5.java
+++ Leetcode/src/main/java/shuai/yu/LongestPalindromicSub_5.java
@@ -108,11 +108,53 @@ public class LongestPalindromicSub_5
      */
     public static String longestPalindrome3(String s)
     {
+        // 特例判断
         if (s == null || s.isEmpty())
             return "";
         if (s.length() == 1)
             return s;
 
-        return s;
+        int maxLen = 1;
+        int begin = 0;
+
+        // dp[i][j] 表示 s[i, j] 是否是回文串
+        boolean[][] dp = new boolean[s.length()][s.length()];
+        char[] charArray = s.toCharArray();
+
+        // 初始化
+        for (int i = 0; i < s.length(); i++)
+        {
+            dp[i][i] = true;
+        }
+
+        for (int j = 1; j < s.length(); j++)
+        {
+            for (int i = 0; i < j; i++)
+            {
+                if (charArray[i] != charArray[j])
+                {
+                    dp[i][j] = false;
+                }
+                else
+                {
+                    if (j - i < 3)
+                    {
+                        dp[i][j] = true;
+                    }
+                    else
+                    {
+                        dp[i][j] = dp[i + 1][j - 1];
+                    }
+                }
+
+                // 只要 dp[i][j] == true 成立，就表示子串 s[i..j] 是回文，此时记录回文长度和起始位置
+                if (dp[i][j] && j - i + 1 > maxLen)
+                {
+                    maxLen = j - i + 1;
+                    begin = i;
+                }
+            }
+        }
+        return s.substring(begin, begin + maxLen);
     }
 }
