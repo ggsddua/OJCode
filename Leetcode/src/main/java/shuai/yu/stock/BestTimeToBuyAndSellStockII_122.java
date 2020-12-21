c4594bde5d16ec6a99e8c4d3ecb459ae0ac54238
shuai.yu_home <shuaiyu0702@foxmail.com>
2020-12-21 21:41:18 +0800

    [动态规划] 股票购买问题

---
+++ Leetcode/src/main/java/shuai/yu/stock/BestTimeToBuyAndSellStockII_122.java
@@ -0,0 +1,49 @@
+package shuai.yu.stock;
+
+/**
+ * 买卖股票的最佳时机 II
+ * <p>
+ * 给定一个数组，它的第 i 个元素是一支给定股票第 i 天的价格。
+ * 设计一个算法来计算你所能获取的最大利润。你可以尽可能地完成更多的交易（多次买卖一支股票）。
+ * 注意: 你不能同时参与多笔交易
+ *
+ * @author shuai.yu
+ * @version 2020/11/08
+ */
+public class BestTimeToBuyAndSellStockII_122 {
+    /**
+     * 贪心算法
+     * 今天和昨天对比，只要能赚，就昨天买今天卖
+     */
+    public int maxProfit(int[] prices) {
+        if (prices == null || prices.length <= 1)
+            return 0;
+        int ans = 0;
+        for (int i = 1; i < prices.length; i++) {
+            int temp = prices[i] - prices[i - 1];
+            if (temp > 0) {
+                ans += temp;
+            }
+        }
+        return ans;
+    }
+
+    /**
+     * 动态规划
+     *
+     * @param prices
+     * @return
+     */
+    public int maxProfit1(int[] prices) {
+        if (prices == null || prices.length <= 1)
+            return 0;
+        int[][] dp = new int[prices.length][2];
+        dp[0][0] = 0;
+        dp[0][1] = -prices[0];
+        for (int i = 1; i < prices.length; i++) {
+            dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i]);
+            dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] - prices[i]);
+        }
+        return dp[prices.length - 1][0];
+    }
+}
