3a50f84b7e7806ad53ce1958e9ed98a6d698652c
shuai.yu_home <shuaiyu0702@foxmail.com>
2020-12-30 22:12:12 +0800

    [Leetcode][动态规划] 股票问题

---
+++ Leetcode/src/main/java/shuai/yu/stock/BestTimeToBuyAndSellStockIIII_188.java
@@ -0,0 +1,52 @@
+package shuai.yu.stock;
+
+import java.util.Arrays;
+
+/**
+ * 条件同上
+ * 你最多可以完成 k 笔交易
+ *
+ * @author shuai.yu
+ * @version 2020/12/21
+ */
+public class BestTimeToBuyAndSellStockIIII_188 {
+    /**
+     * 使用123题解决方案，将所有状态列出来
+     */
+    public static int maxProfit(int k, int[] prices) {
+        int len = prices.length;
+        if (len <= 1 || k<=0)
+            return 0;
+        int[][] dp = new int[len][k * 2 + 1];
+
+        // 动态规划入口
+        for (int i = 0; i < dp[0].length; i++) {
+            if (i % 2 == 0)
+                dp[0][i] = 0;
+        }
+        dp[0][1] = -prices[0];
+
+        // 状态转化过程
+        for (int i = 1; i < len; i++) {
+            // 一次没买，收益为0
+            dp[i][0] = 0;
+            for (int j = 1; j < dp[i].length; j++) {
+                if (j % 2 == 0) {
+                    // 第i天买了又卖的收益
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - 1] + prices[i]);
+                } else {
+                    // 第i天买了的收益
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - 1] - prices[i]);
+                }
+            }
+        }
+        // 找到最后一天的最大收益
+        int ans = Integer.MIN_VALUE;
+        for (int i : dp[len - 1]) {
+            if (ans < i) {
+                ans = i;
+            }
+        }
+        return ans;
+    }
+}
---
+++ Leetcode/src/main/java/shuai/yu/stock/BestTimeToBuyAndSellStockIIWithFee_714.java
@@ -0,0 +1,23 @@
+package shuai.yu.stock;
+
+/**
+ * 带手续费的
+ *  在122题的基础上，加入手续费
+ * @author shuai.yu
+ * @version 2020/12/22
+ */
+public class BestTimeToBuyAndSellStockIIWithFee_714 {
+    public int maxProfit(int[] prices, int fee) {
+        if (prices == null || prices.length <= 1)
+            return 0;
+        int[][] dp = new int[prices.length][2];
+        dp[0][0] = 0;
+        dp[0][1] = -prices[0];
+        for (int i = 1; i < prices.length; i++) {
+            // 正常的股票购买动态规划，在卖出时收取手续费即可
+            dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i] - fee);
+            dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] - prices[i]);
+        }
+        return dp[prices.length - 1][0];
+    }
+}
